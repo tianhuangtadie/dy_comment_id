8a5a4a7eb744365bcae54f7a9a8bc0ce2faaed46
Go.mxy <34773306+tianhuangtadie@users.noreply.github.com>
2025-01-09 16:54:06 +0800

    Update content.js

--- content.js
+++ content.js
@@ -87,7 +87,7 @@ copyOption.addEventListener("click", () => {
         const cleanedId = rawId.replace("tooltip_", "");
         // 复制到剪切板
         navigator.clipboard.writeText(cleanedId).then(() => {
-            alert("节点信息已复制到剪切板！");
+            Toast("评论ID已提取到剪切板！", 1000);
         }).catch((err) => {
             console.error("复制失败:", err);
         });
@@ -96,3 +96,20 @@ copyOption.addEventListener("click", () => {
         contextMenu.style.display = "none";
     }
 });
+
+
+function Toast(msg, duration) {
+    duration = isNaN(duration) ? 3000 : duration;
+    var m = document.createElement('div');
+    m.innerHTML = msg;
+    m.style.cssText = "font-family:siyuan;max-width:60%;min-width: 150px;padding:0 14px;height: 40px;color: rgb(255, 255, 255);line-height: 40px;text-align: center;border-radius: 4px;position: fixed;top: 50%;left: 50%;transform: translate(-50%, -50%);z-index: 999999;background: rgba(0, 0, 0,.7);font-size: 16px;";
+    document.body.appendChild(m);
+    setTimeout(function () {
+        var d = 0.5;
+        m.style.webkitTransition = '-webkit-transform ' + d + 's ease-in, opacity ' + d + 's ease-in';
+        m.style.opacity = '0';
+        setTimeout(function () {
+            document.body.removeChild(m)
+        }, d * 1000);
+    }, duration)
+}
